bef9337127858ebc4facfe851b265d739941ded7
administrator <2316696377@qq.com>
2020-05-15 18:14:55 +0800

    nacos-server

--- cloud-nacos-consumer83/src/main/java/com/fy/springcloud/nacos/consumer/controller/NacosController.java
+++ cloud-nacos-consumer83/src/main/java/com/fy/springcloud/nacos/consumer/controller/NacosController.java
@@ -1,18 +1,24 @@
-package com.fy.springcloud.nacos.controller;
+package com.fy.springcloud.nacos.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 public class NacosController {
 
-    @Value("${server.port}")
-    private Integer port;
+    @Value("${service-url.nacos-user-service}")
+    private String url;
+
+    @Resource
+    private RestTemplate restTemplate;
 
     @GetMapping("/nacos/{string}")
     public String nacos(@PathVariable String string){
-        return  port + " - hello, nacos" + string;
+        return restTemplate.getForObject(url + "/nacos/" + string, String.class);
     }
 }
--- cloud-nacos-server9001/src/main/java/com/fy/springcloud/nacos/controller/NacosController.java
+++ cloud-nacos-server9001/src/main/java/com/fy/springcloud/nacos/controller/NacosController.java
@@ -1,14 +1,17 @@
 package com.fy.springcloud.nacos.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NacosController {
+    @Value("${server.port}")
+    private Integer port;
 
     @GetMapping("/nacos/{string}")
     public String nacos(@PathVariable String string){
-        return "hello, nacos" + string;
+        return  port + " - hello, nacos" + string;
     }
 }
