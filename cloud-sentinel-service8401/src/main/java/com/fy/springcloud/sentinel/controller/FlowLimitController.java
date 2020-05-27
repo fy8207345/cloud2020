9645b0731aa878ed6f08e122a4e90ac057e80841
administrator <2316696377@qq.com>
2020-05-27 10:52:04 +0800

    sentinel

--- cloud-sentinel-service8401/src/main/java/com/fy/springcloud/sentinel/controller/FlowLimitController.java
+++ cloud-sentinel-service8401/src/main/java/com/fy/springcloud/sentinel/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.fy.springcloud.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,16 @@ public class FlowLimitController {
     @GetMapping("/flowB")
     public String flowB(){
         return "flowB";
+    }
+
+    @GetMapping("/hot")
+    @SentinelResource(value = "hot", blockHandler = "hot_Handler")
+    public String hot(@RequestParam(value = "p1", required = false) String p1,
+                      @RequestParam(value = "p2", required = false) String p2){
+        return "hot";
+    }
+
+    public String hot_Handler(String p1, String p2, BlockException e){
+        return "hot_Handler:" + e.getMessage();
     }
 }
